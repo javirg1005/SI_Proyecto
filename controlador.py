95b7530ff4d7616e7156b765fb05ae79d21dda7d
mariasol210 <mariasol210@hotmail.com>
2021-12-28 16:00:08 +0100

    vista de busqueda y funcion del modelo con vista

--- controlador.py
+++ controlador.py
@@ -1,19 +1,99 @@
 import sys
 from PyQt5 import QtWidgets, uic
 import re
-from PyQt5.QtWidgets import QTableWidgetItem, QHeaderView
+import os
+from modelo.DocProcessing import DocProcessing
 
 class MyWindow(QtWidgets.QMainWindow):
 
     #El orden importa, para cargar datos importa, aqui cargar los combobox
-
     def __init__(self):
         super(MyWindow,self).__init__()
-        uic.loadUi('interfaz.ui',self)      
+        uic.loadUi('./vista/main.ui',self)    
+
+        self.fill_combobox()
+        self.ranking_paths = []
 
         #hay que ir añadiendo los elementos de la ventana
         #ejemplo:
         #self.cb_fuente.addItems()
+
+    def fill_combobox(self):
+        self.cb_fuente.addItems(["Escoger fuente", "20minutos", "elmundo", "elpais"])
+        self.cb_filtro_fuente.addItems(["Escoger fuente", "20minutos", "elmundo", "elpais"])
+        self.cb_filtro_fuente_1.addItems(["Escoger fuente", "20minutos", "elmundo", "elpais"])
+        self.cb_category.addItems(["Escoger categoría", "ciencia", "salud", "tecnologia"])
+        self.cb_filtro_categ.addItems(["Escoger categoría", "ciencia", "salud", "tecnologia"])
+        self.cb_new.addItem("Escoger noticia")
+        self.cb_top.addItems(['1','2','3','4','5','6','7','8','9','10'])
+        self.cb_top_1.addItems(['1','2','3','4','5','6','7','8','9','10'])
+
+        self.cb_fuente.currentTextChanged.connect(self.fill_cb_news_files)
+        self.cb_category.currentTextChanged.connect(self.fill_cb_news_files)
+        self.cb_new.currentTextChanged.connect(self.on_cb_new_changed)
+        self.btn_search.clicked.connect(self.search_callback)
+        self.btn_search_1.clicked.connect(self.search_1_callback)
+        self.ql_ranking_1.itemClicked.connect(self.ranking_clicked)
+
+    def fill_cb_news_files(self):
+        # fill every news txt name
+        fuente = self.cb_fuente.currentText()
+        categoria = self.cb_category.currentText()
+        if (fuente == "Escoger fuente" or categoria == "Escoger categoría"):
+            self.tx_preview.clear()
+            self.cb_new.clear()
+            self.cb_new.addItem("Escoger noticia")
+        else:
+            self.tx_preview.clear()
+            self.cb_new.clear()
+            self.cb_new.addItem("Escoger noticia")
+            file_dir_path = os.path.dirname(__file__)
+            project_dir_path = os.path.dirname(file_dir_path)
+            for dirpath, dirs, files in os.walk(project_dir_path + "\\noticias\\"+ fuente + "\\" + categoria):
+                for f in files:
+                    self.cb_new.addItem(f)
+            
+
+    def on_cb_new_changed(self, value):
+        fuente = self.cb_fuente.currentText()
+        categoria = self.cb_category.currentText()
+        if (value == "Escoger noticia" or fuente == "Escoger fuente" or categoria == "Escoger categoría" or value == ""):
+            self.tx_preview.clear()
+        else:
+            print("combobox changed", value)
+            path = "./noticias/" + fuente + '/'+ categoria + '/'+ value
+            with open(path, encoding="utf-8") as pearl:
+                text = pearl.read()
+            self.tx_preview.insertPlainText(text)
+
+    def search_callback(self):
+        #PONER LA FUNCION DE BUSCAR SEGUN LA NOTICIA (punto 2)
+        pass
+
+    def search_1_callback(self):
+        #PONER LA FUNCION DE BUSCAR SEGUN QUERY (punto 1)
+        query = self.tx_query.toPlainText()
+        print(query)
+        if query != "":
+            process = DocProcessing()
+            top = int(self.cb_top_1.currentText())
+            sim = process.query_sim_ranking(query, top)
+            self.ql_ranking_1.clear()
+            self.ranking_paths.clear()
+            for key in list(sim.keys()):
+                self.ranking_paths.append(str(key))
+                text = str(sim[key]) + ' - ' + str(key).split('\\').pop()
+                item = QtWidgets.QListWidgetItem(text)
+                self.ql_ranking_1.addItem(item)
+
+    def ranking_clicked(self, item):
+        item_ind = self.ql_ranking_1.row(item)
+        self.tx_noticia_1.clear()
+        print('item clicked:',self.ranking_paths[item_ind] )
+        with open(self.ranking_paths[item_ind], encoding="utf-8") as pearl:
+                text = pearl.read()
+        self.tx_noticia_1.insertPlainText(text)
+
 
     # para cargar datos importa, aqui cargar los botones
 
--- modelo/DocProcessing.py
+++ modelo/DocProcessing.py
@@ -1,6 +1,4 @@
-import string
 import os
-import re
 import gensim
 from nltk.tokenize import word_tokenize
 from nltk.stem import SnowballStemmer
@@ -8,12 +6,14 @@ from nltk.stem import SnowballStemmer
 class DocProcessing:
 
     def __init__(self):
-        f = open("lista_parada.txt", "r", encoding="utf-8",)
+        f = open("modelo\lista_parada.txt", "r", encoding="utf-8",)
         self.stop_words = f.read().split('\n')
         f.close()
         # read files and store in dic. Then tokenize and stem each one
         self.docs = self.read_docs()
-        self.docs_stems = self.tokenize_and_stem_docs(self.docs.values())
+        self.docs_names = list(self.docs.keys())
+        self.docs_text = self.docs.values()
+        self.docs_stems = self.tokenize_and_stem_docs(self.docs_text)
 
         # Create dictionary of stems
         self.dictionary = gensim.corpora.Dictionary(self.docs_stems)
@@ -23,29 +23,29 @@ class DocProcessing:
         self.tf_idf = gensim.models.TfidfModel(self.corpus)
 
         # Create a similarity measure object
-        self.sims = gensim.similarities.Similarity('similarity_object/', self.tf_idf[self.corpus], num_features=len(self.dictionary))s
+        self.sims = gensim.similarities.Similarity('modelo\\similarity_object\\', self.tf_idf[self.corpus], num_features=len(self.dictionary))
 
-    def read_docs():
+    def read_docs(self):
         token_dict = {}
-        # I get the absolute path of the current file to get the path of the directory of the project
-        absolute_path = os.path.abspath(__file__)
-        project_dir_path = os.path.dirname(absolute_path)
+        # I get the directory path of the current file to later get the path of the directory of the project
+        file_dir_path = os.path.dirname(__file__)
+        project_dir_path = os.path.dirname(file_dir_path)
         # I use the project directory path to build the path for the news folder
         news_path = project_dir_path + "\\noticias"
         # I navigate through the directories to get all news files
         for dirpath, dirs, files in os.walk(news_path):
             for f in files:
                 fname = os.path.join(dirpath, f)
-                print("fname=", fname)
+                #print("fname=", fname)
                 with open(fname, encoding="utf-8") as pearl:
-                    text = pearl.read().lower()
-                    token_dict[f] = re.sub("[" + string.punctuation + "¿¡“”]", "", text)
+                    text = pearl.read()
+                    token_dict[fname] = text
 
         return token_dict
 
     def tokenize_and_stem_docs(self, docs):
         # Tokenize and filter stop words
-        gen_docs = [[w.lower() for w in word_tokenize(text_doc) if w not in self.stop_words + list(string.punctuation) + ['¿','¡','“','”'] ] 
+        gen_docs = [[w.lower() for w in word_tokenize(text_doc) if w not in self.stop_words] 
                 for text_doc in docs]
         stems = []
         stemmer = SnowballStemmer("spanish")
@@ -60,7 +60,7 @@ class DocProcessing:
 
     def tokenize_and_stem_query(self, query):
         # Tokenize and filter stop words
-        query_doc = [w.lower() for w in word_tokenize(query) if w not in self.stop_words + list(string.punctuation) + ['¿','¡','“','”']]
+        query_doc = [w.lower() for w in word_tokenize(query) if w not in self.stop_words]
         stems = []
         stemmer = SnowballStemmer("spanish")
         #Stemming
@@ -79,9 +79,22 @@ class DocProcessing:
         # perform a similarity query against the corpus
         return self.sims[query_doc_tf_idf]
 
+    def query_sim_ranking(self, query, ranking):
+        sim = self.query_sim(query)
+        rank = sim.argsort()[-ranking:]
+        rank_dic = {}
+        for i in reversed(rank):
+            rank_dic[self.docs_names[i]] = sim[i]
+
+        return rank_dic        
 
 
-    #query similirarity
-    query = "Saturno es el sexto planeta desde el sol"
-    print(query_sim(query, dictionary, sims))
+'''process = DocProcessing()
+print("ranking")
 
+query = "Saturno es el sexto planeta desde el sol"
+print(process.query_sim_ranking(query, 5))
+
+query = "Alexa puede detectar por la voz el estado emocional de los pacientes en algunos hospitales españoles"
+print(process.query_sim_ranking(query, 5))
+'''
